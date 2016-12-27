5d9bb7c970e0c12d014476b0b4bfd7884a394687
kvm94 <kevin.marra94@hotmail.fr>
2016-12-27 14:02:23 +0100

    ADD: TOP 10

---
+++ app/src/main/java/be/condorcet/marra/scores/RPC/TopAsync.java
@@ -0,0 +1,125 @@
+package be.condorcet.marra.scores.RPC;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.JsonReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import be.condorcet.marra.scores.TopActivity;
+
+
+public class TopAsync extends AsyncTask<String, Void, ArrayList<String[]>> {
+
+//Attributs
+
+    private TopActivity screen;
+    private int code;
+
+    public TopAsync(TopActivity screen) {
+        this.screen = screen;
+    }
+
+    private ProgressDialog progDailog;
+
+    //Initialise la barre de chargement.
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progDailog = new ProgressDialog(screen);
+        progDailog.setMessage("Loading...");
+        progDailog.setIndeterminate(false);
+        progDailog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progDailog.setCancelable(true);
+        progDailog.show();
+    }
+
+    //Récupère les données du RPC
+    @Override
+    protected ArrayList<String[]> doInBackground(String... data) {
+
+        // Exécution en arrière-plan
+        ArrayList<String[]> responses = new ArrayList<String[]>();
+        String[] response;
+
+
+        try {
+
+            String game = data[0];
+
+            //Convertit les espace dans la requête GET;
+            game = game.replace(" ", "%20");
+
+            //Lance le script php du serveur
+
+            URL url = new URL("http://www.kmarra.be/rpc/afficher_top.php?jeu=" + game);
+            HttpURLConnection connection;
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestProperty("Content-Type", "text/plain");
+            connection.setRequestProperty("charset", "utf-8");
+
+            connection.setConnectTimeout(1000);
+            connection.connect();
+
+
+            //Récupère les données du JSON
+
+            if (connection.getResponseCode() == 200) {
+
+                InputStream inputStream = connection.getInputStream();
+                connection.setReadTimeout(5000);
+                InputStreamReader inputStreamReader;
+                inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+
+                JsonReader json_reader = new JsonReader(inputStreamReader);
+                json_reader.beginObject();
+                while (json_reader.hasNext()) {
+
+                    if (json_reader.nextName().equals("code")) {
+                        code = json_reader.nextInt();
+                    }
+
+                    if (json_reader.nextName().equals("joueurs")) {
+                        json_reader.beginArray();
+
+                        while (json_reader.hasNext()) {
+                            json_reader.beginObject();
+                            response = new String[2];
+
+                            if (json_reader.nextName().equals("pseudo")) {
+                                response[0] = json_reader.nextString();
+                            }
+                            if (json_reader.nextName().equals("score")) {
+                                response[1] = json_reader.nextString();
+                            }
+                            responses.add(response);
+                            json_reader.endObject();
+                        }
+                    }
+                    json_reader.endArray();
+                }
+                json_reader.endObject();
+                json_reader.close();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return responses;
+    }
+
+    //Termine la tâche.
+    @Override
+    protected void onPostExecute(ArrayList<String[]> result) {
+        // Callback
+        progDailog.dismiss();
+        screen.responseAsync(result, code);
+    }
+}
--- app/src/main/java/be/condorcet/marra/scores/TopActivity.java
+++ app/src/main/java/be/condorcet/marra/scores/TopActivity.java
@@ -1,13 +1,110 @@
 package be.condorcet.marra.scores;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import be.condorcet.marra.scores.RPC.AddScoreAsync;
+import be.condorcet.marra.scores.RPC.TopAsync;
 
 public class TopActivity extends AppCompatActivity {
+
+    //Attributs
+
+    private final int GAMES_REQUEST = 3;
+
+    private Button btn_select;
+    private Button btn_games;
+    private EditText et_nameGame;
+
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+
+        //Initialise contenu.
+
+        list = (ListView)findViewById(R.id.topList);
+        btn_games = (Button)findViewById(R.id.btn_games);
+        btn_games.setOnClickListener(listener_btn_games);
+        btn_select = (Button)findViewById(R.id.btn_selectGame);
+        btn_select.setOnClickListener(listener_btn_select);
+        et_nameGame = (EditText)findViewById(R.id.et_nameGame);
+
+        //Pour la liste dynamique.
+        items = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(TopActivity.this, android.R.layout.simple_list_item_1, items);
+        list.setAdapter(adapter);
+
     }
+
+    //Listener
+
+    //Lance le wizard de selection de jeu.
+    private View.OnClickListener listener_btn_games = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Intent intent = new Intent(TopActivity.this, GamesWizard.class);
+            startActivityForResult(intent, GAMES_REQUEST );
+        }
+
+    };
+
+    private View.OnClickListener listener_btn_select = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            try{
+                String game = et_nameGame.getText().toString();
+                new TopAsync(TopActivity.this).execute(game);
+            }
+            catch (Exception ex){
+                Alert.showSimpleErrorAlert(TopActivity.this, ex.getMessage());
+            }
+        }
+
+    };
+
+    /**
+     * Recovers data from GamesWizard.
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == GAMES_REQUEST){
+            if(resultCode == RESULT_OK){
+                et_nameGame.setText(data.getStringExtra("game"));
+            }
+        }
+
+    }
+
+    public void responseAsync(ArrayList<String[]> result, int code){
+        switch (code){
+            case 0:
+
+                items.clear();
+                for (int i=0;i< result.size() ; i++) {
+                    items.add((i+1) + ") " + result.get(i)[0] + " : " + result.get(i)[1] );
+                }
+
+                adapter.notifyDataSetChanged();
+
+                break;
+            default:
+                Alert.showSimpleAlert(TopActivity.this, getString(R.string.unknownError) + " (code:" + code + ")");
+                break;
+        }
+    }
+
 }
